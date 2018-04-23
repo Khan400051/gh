2ebc5e4477d6a9d41dfcb46af989392b26736efa
MSZ <msz_900@yahoo.com>
2018-04-23 14:20:13 +0500

    Changes made

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>This is the Index File and I am practcing it for Git Hub.</p>
+    </body>
+</html>
