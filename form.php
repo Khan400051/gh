3f254e1bf7e1b7f99201f0f21a4128888761ae58
MSZ <msz_900@yahoo.com>
2018-04-23 14:23:31 +0500

    Form.php page is added

---
+++ form.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Form | Made</title>
+    </head>
+    <body>
+        <form action="#">
+            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Email">
+            <input type="text" placeholder="Password">
+            <input type="submit" name="Login" id="Login">
+        </form>
+    </body>
+</html>
